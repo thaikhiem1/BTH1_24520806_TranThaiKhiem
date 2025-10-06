62d0be40ee8466774ae71d703bc0e9dd5d155556
thaikhiem1 <tranthaikhiem321@gmail.com>
2025-10-06 14:45:16 +0700

    Update Program.cs

--- Bai06/Bai06/Program.cs
+++ Bai06/Bai06/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Bai06
 {
     internal class Program
     {
-        //câu a: Xuất ma trận
-        static void Xuatmatrix(int[,] mt)
+        // Câu a: Xuất ma trận
+        static void xuatmatrix(int[,] mt)
         {
             for (int i = 0; i < mt.GetLength(0); i++)
             {
@@ -20,8 +17,9 @@ namespace Bai06
                 Console.WriteLine();
             }
         }
-        //câu b:tìm phần tử lớn nhất/nhỏ nhất;
-        static int Timmax(int[,] mt)
+
+        // Câu b: tìm phần tử lớn nhất/nhỏ nhất
+        static int timmax(int[,] mt)
         {
             int maxValue = mt[0, 0];
             for (int i = 0; i < mt.GetLength(0); i++)
@@ -29,14 +27,12 @@ namespace Bai06
                 for (int j = 0; j < mt.GetLength(1); j++)
                 {
                     if (mt[i, j] > maxValue)
-                    {
                         maxValue = mt[i, j];
-                    }
                 }
             }
             return maxValue;
         }
-        static int Timmin(int[,] mt)
+        static int timmin(int[,] mt)
         {
             int minValue = mt[0, 0];
             for (int i = 0; i < mt.GetLength(0); i++)
@@ -44,16 +40,13 @@ namespace Bai06
                 for (int j = 0; j < mt.GetLength(1); j++)
                 {
                     if (mt[i, j] < minValue)
-                    {
                         minValue = mt[i, j];
-                    }
                 }
             }
             return minValue;
-
         }
-        //câu c:tìm dòng có tổng lớn nhất:
-        static int LineSumMax(int[,] mt)
+        // Câu c: tìm dòng có tổng lớn nhất
+        static int linesummax(int[,] mt)
         {
             int LineMax = 0;
             int SumMax = int.MinValue;
@@ -61,9 +54,7 @@ namespace Bai06
             {
                 int tong = 0;
                 for (int j = 0; j < mt.GetLength(1); j++)
-                {
                     tong += mt[i, j];
-                }
                 if (tong > SumMax)
                 {
                     LineMax = i;
@@ -71,22 +62,13 @@ namespace Bai06
                 }
             }
             return LineMax;
-
         }
-        //câu d: Tính tổng các số không là số nguyên tố:
-        static bool Isprime(int n)
+        // Câu d: Tính tổng các số không là số nguyên tố
+        static bool isprime(int n)
         {
-            if (n < 2)
-            {
-                return false;
-            }
+            if (n < 2) return false;
             for (int i = 2; i <= Math.Sqrt(n); i++)
-            {
-                if (n % i == 0)
-                {
-                    return false;
-                }
-            }
+                if (n % i == 0) return false;
             return true;
         }
         static int SumNoPrime(int[,] mt)
@@ -96,55 +78,46 @@ namespace Bai06
             {
                 for (int j = 0; j < mt.GetLength(1); j++)
                 {
-                    if (!Isprime(mt[i, j]))
-                    {
+                    if (!isprime(mt[i, j]))
                         Sum += mt[i, j];
-                    }
                 }
             }
             return Sum;
         }
-        //câu e: xóa dòng thứ k;
-        static int[,] XoaDongK(int[,] mt, int k)
+        // Câu e: Xóa dòng thứ k trực tiếp trên ma trận gốc
+        static void xoadongk(ref int[,] mt, int k)
         {
-            try
+            if (k < 0 || k >= mt.GetLength(0))
             {
-                if (k < 0 || k >= mt.GetLength(0))
-                {
-                    throw new ArgumentOutOfRangeException(nameof(k), "Chỉ số dòng k không hợp lệ!");
-                }
-                int[,] ans = new int[mt.GetLength(0) - 1, mt.GetLength(1)];
-                int r = 0;
-                for (int i = 0; i < mt.GetLength(0); i++)
-                {
-                    if (i == k)
-                    {
-                        continue;
-                    }
-                    for (int j = 0; j < mt.GetLength(1); j++)
-                    {
-                        ans[r, j] = mt[i, j];
-                    }
-                    r++;
-                }
-                return ans;
+                Console.WriteLine("Chỉ số dòng k không hợp lệ!");
+                return;
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Lỗi: " + ex.Message);
-                return mt;
-            }
-        }
-        //câu f: Xóa cột chứa phần tử lớn nhất trong ma trận:
-        static int[,] XoaCotMax(int[,] mt)
-        {
             int rows = mt.GetLength(0);
             int cols = mt.GetLength(1);
-            int maxValue = mt[0, 0];
-            int maxCot = 0;
+            int[,] temp = new int[rows - 1, cols];
+            int r = 0;
+
             for (int i = 0; i < rows; i++)
             {
+                if (i == k) continue;
                 for (int j = 0; j < cols; j++)
+                    temp[r, j] = mt[i, j];
+                r++;
+            }
+            mt = temp; 
+        }
+
+        // Câu f: Xóa cột chứa phần tử lớn nhất
+        static int[,] xoacotmax(int[,] mt)
+        {
+            int row = mt.GetLength(0);
+            int col = mt.GetLength(1);
+            int maxValue = mt[0, 0];
+            int maxCot = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
                 {
                     if (mt[i, j] > maxValue)
                     {
@@ -153,16 +126,13 @@ namespace Bai06
                     }
                 }
             }
-            int[,] ans = new int[rows, cols - 1];
-            for (int i = 0; i < rows; i++)
+            int[,] ans = new int[row, col - 1];
+            for (int i = 0; i < row; i++)
             {
                 int c = 0;
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < col; j++)
                 {
-                    if (j == maxCot)
-                    {
-                        continue;
-                    }
+                    if (j == maxCot) continue;
                     ans[i, c] = mt[i, j];
                     c++;
                 }
@@ -171,35 +141,42 @@ namespace Bai06
         }
         static void Main(string[] args)
         {
-            Console.InputEncoding = Encoding.UTF8;
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.Write("Nhập số hàng: ");
-            int m = int.Parse(Console.ReadLine());
-            Console.Write("Nhập số cột: ");
-            int n = int.Parse(Console.ReadLine());
-            Random rd = new Random();
-            int[,] matrix = new int[m, n];
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            try
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                Console.InputEncoding = Encoding.UTF8;
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.Write("Nhập số hàng: ");
+                int m = int.Parse(Console.ReadLine());
+                Console.Write("Nhập số cột: ");
+                int n = int.Parse(Console.ReadLine());
+                Random rd = new Random();
+                int[,] matrix = new int[m, n];
+                for (int i = 0; i < matrix.GetLength(0); i++)
                 {
-                    matrix[i, j] = rd.Next(-100, 101);
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = rd.Next(-100, 101);
+                    }
+
                 }
+                Console.WriteLine("Ma trận vừa tạo: ");
+                xuatmatrix(matrix);
+                Console.WriteLine("Số lớn nhất trong ma trận: " + timmax(matrix));
+                Console.WriteLine("Số nhỏ nhất trong ma trận: " + timmin(matrix));
+                Console.WriteLine("Dòng có tổng lớn nhất: " + linesummax(matrix));
+                Console.WriteLine("Tổng các số không là nguyên tố: " + SumNoPrime(matrix));
+                Console.Write("Nhập k: ");
+                int k = int.Parse(Console.ReadLine());
+                xoadongk(ref matrix, k);
+                Console.WriteLine($"Ma trận sau khi xóa dòng {k}: ");
+                xuatmatrix(matrix);
+                matrix = xoacotmax(matrix);
+                Console.WriteLine("Ma trận sau khi xóa cột có số lớn nhất: ");
+                xuatmatrix(matrix);
             }
-            Console.WriteLine("Ma trận vừa tạo: ");
-            Xuatmatrix(matrix);
-            Console.WriteLine("Số lớn nhất trong ma trận: " + Timmax(matrix));
-            Console.WriteLine("Số nhỏ nhất trong ma trận: " + Timmin(matrix));
-            Console.WriteLine("Dòng có tổng lớn nhất: " + LineSumMax(matrix));
-            Console.WriteLine("Tổng các số không là nguyên tố: " + SumNoPrime(matrix));
-            Console.Write("Nhập k: ");
-            int k = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Ma trận sao khi xóa dòng {k}: ");
-            int[,] ans = XoaDongK(matrix, k);
-            Xuatmatrix(ans);
-            int[,] result = XoaCotMax(matrix);
-            Console.WriteLine($"Ma trận sao khi xóa cột có số lớn nhất: ");
-            Xuatmatrix(result);
+            catch (Exception ex) { 
+                  Console.WriteLine(ex.Message);
+            }
         }
     }
 }
